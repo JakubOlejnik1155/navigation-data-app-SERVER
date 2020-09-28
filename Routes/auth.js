044be515b8dcec990c4d34761910bbd06772eaa7
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-09-28 10:45:15 +0200

    add synchronize data breakpoint

--- Routes/auth.js
+++ Routes/auth.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const verify = require('./verifyToken')
 
+//register new user
 router.post('/register' , async(req, res) => {
     //validation
     const {error} = registerValidation(req.body);
@@ -46,6 +47,7 @@ router.post('/register' , async(req, res) => {
     }
 })
 
+//login user
 router.post('/login', async (req, res) => {
     const {error} = loginValidation(req.body);
     if (error)
@@ -74,9 +76,23 @@ router.post('/login', async (req, res) => {
     res.send({ok: true, status: 200, user: userObject, jwt: token});
 })
 
+//get online data
 router.get('/', verify,async (req, res) => {
     const userObject = await UserModel.findOne({_id: req.user._id}, '-password')
     res.send({ok: true, code: 200, object: userObject});
+})
+
+//synchronize data
+router.patch('/', verify, async (req, res) => {
+    try{
+        const userObject = await UserModel.findOne({_id: req.user._id})
+        await userObject.updateOne({harborsArray: req.body.harborsArray, tripsArray: req.body.tripsArray, log: req.body.log});
+        await userObject.save()
+        res.send({ok: true, code: 200})
+    }catch (e) {
+        res.status(400).send({error: true, code: 400, msg: "something get wrong"})
+    }
+
 })
 
 module.exports = router;
