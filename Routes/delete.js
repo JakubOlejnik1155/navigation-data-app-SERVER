38f7442a1863777c9217a718f3927f91a623ea30
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-09-28 15:39:20 +0200

    delete harbor breakpoint

---
+++ Routes/delete.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const UserModel = require('../model/User');
+const verify = require('./verifyToken')
+
+
+router.delete('/harbor/:name', verify, async (req, res) =>{
+    const harborName = req.params.name;
+    try{
+        const userObject = await UserModel.findOne({_id: req.user._id})
+        const array = userObject.harborsArray;
+        let newArray = []
+        for (let i = 0 ; i < array.length ; i++){
+            if (array[i].name !== harborName){
+                newArray.push(array[i]);
+            }
+        }
+        await userObject.updateOne({harborsArray: newArray});
+        await userObject.save()
+        res.send({ok: true, code: 200})
+    }catch (e) {
+        res.status(400).send({error: true, code: 400, msg: "something get wrong"})
+    }
+})
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./Routes/auth');
-
+const deleteRoutes = require('./Routes/delete')
 const app = express();
 app.use(express.json());
 app.use(cors())
@@ -21,6 +21,7 @@ mongoose.connect(process.env.DB_CONNECT,
 
 //route middlewares
 app.use('/api/user', authRoutes);
+app.use('/api', deleteRoutes);
 
 
 //start listening the server
