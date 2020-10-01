325753c81d85300dad223a615a0a8ccc5616a5fc
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-10-01 10:34:27 +0200

    add delete trip breakpoint

--- Routes/delete.js
+++ Routes/delete.js
@@ -22,5 +22,24 @@ router.delete('/harbor/:name', verify, async (req, res) =>{
     }
 })
 
+router.delete('/trip/:startTime', verify , async (req, res) => {
+    const tripStartTime = req.params.startTime;
+    try {
+        const userObject = await UserModel.findOne({_id: req.user._id});
+        const array = userObject.tripsArray;
+        let newArray = [];
+        for (let i = 0; i < array.length ; i++){
+            if (array[i].startTime.toJSON() !== new Date(tripStartTime).toJSON()){
+                newArray.push(array[i])
+            }
+        }
+        await userObject.updateOne({tripsArray: newArray})
+        await userObject.save();
+        res.send({ok: true, status: 200})
+    }catch (e) {
+        res.status(400).send({error: true, code: 400, msg: "something get wrong"})
+    }
+})
+
 
 module.exports = router;
