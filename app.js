6c7aef49cf501e5b80befeb08016d494fadd7e97
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-10-12 10:27:41 +0200

    add limit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.DB_CONNECT,
     ,()=>{
     console.log("Connected to DB")
 })
-app.use(express.json());
+app.use(express.json({ limit: '50mb', extended: true }));
 app.use(cors())
 app.use(express.static('build', {}))
 //routes middlewares
